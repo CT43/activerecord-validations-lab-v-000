e6e291fe17571cec28d445fe24e8a4803546f8f7
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-27 19:14:36 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,10 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :validate_clickbait
 
+
+
   def validate_clickbait
     #self.title.include?(["Won't Believe", "Secret", "Top #{/[0-9]*/}", "Guess"])
-    ["Won't Believe", "Secret", "Top #{/[0-9]*/}", "Guess"].any? { |clickbait| self.title.include? clickbait }
+    ["Won't Believe", "Secret", "Top #{/[0-9]*/}", "Guess"].any? { |clickbait| title.include? clickbait }
   end
 end
