801ae3a1cc67d86bb6a9b23283b85c3ab0a54c46
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-27 19:05:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :validate_clickbait
+
+  def validate_clickbait
+    if self.title.include?()
+
+  end
 end
